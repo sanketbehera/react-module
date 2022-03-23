2c8794715b76a5e22299c02c50e07dd598db9d85
sk-ismail <isk98073@gmail.com>
2022-03-23 12:24:08 +0530

    updated build files

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1,21 +1,37 @@
 import _slicedToArray from '@babel/runtime/helpers/slicedToArray';
-import React, { useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 var MfeButton = function MfeButton(props) {
+  var timerHandle = useRef(null);
+
   var _useState = useState(0),
       _useState2 = _slicedToArray(_useState, 2),
-      result = _useState2[0],
-      setResult = _useState2[1];
+      stateCounter = _useState2[0],
+      setStateCounter = _useState2[1];
 
-  var fromMfe = function fromMfe(data) {
-    props.checkMe(data);
+  useEffect(function () {
+    timerHandle.current = +setInterval(function () {
+      setStateCounter(stateCounter + 1);
+    }, 2500);
+    return function () {
+      if (timerHandle.current) {
+        clearInterval(timerHandle.current);
+        timerHandle.current = null;
+      }
+    };
+  });
+  var propsCounter = props.counter,
+      onClick = props.onClick;
+
+  var handleClick = function handleClick() {
+    if (onClick) {
+      onClick();
+    }
   };
 
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("button", {
-    onClick: function onClick(e) {
-      setResult(result + 1), fromMfe(result);
-    }
-  }, "MFE1 Button"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("span", null, result));
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("div", null, "Props counter: ", propsCounter, /*#__PURE__*/React.createElement("button", {
+    onClick: handleClick
+  }, "click to increase")), /*#__PURE__*/React.createElement("div", null, "State counter: ", stateCounter));
 };
 
 export { MfeButton as default };
